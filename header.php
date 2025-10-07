66e7719e29b8ea80c98282754bab140874e43202
OJhonnys <ojhonnydasilva@gmail.com>
2025-10-06 21:10:02 -0300

    first commit

---
+++ header.php
@@ -0,0 +1,40 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Ultra Construction</title>
+    
+    <link rel="stylesheet" href="assets/css/style.css">
+</head>
+<body>
+    <header class="site-header">
+        <div class="container-header">
+            <div class="logo">
+                <a href="index.php"><img src="assets/images/logo.png" alt="Ultra Construction Logo"></a>
+            </div>
+
+            <nav class="main-nav">
+                <ul>
+                    <li><a href="index.php">Home</a></li>
+                    <li><a href="projetos.php">Projects</a></li>
+                    <li><a href="sobre.php">About Us</a></li>
+                    <li><a href="contato.php">Contact Us</a></li>
+                </ul>
+            </nav>
+
+            <div class="header-right">
+                <div class="header-top">
+                    <a href="954.533.5572" class="header-phone">954.533.5572</a>
+                    <a href="https://www.facebook.com" target="_blank"><img src="assets/images/Facebook.png" alt="Facebook Logo" class="icon-small"></a>
+                    <a href="https://www.instagram.com" target="_blank"><img src="assets/images/Instagram.png" alt="Instagram Logo" class="icon-small"></a>
+                    <a href="https://www.linkedin.com" target="_blank"><img src="assets/images/Linkedin.png" alt="Linkedin Logo" class="icon-small"></a>
+                </div>
+                <div class="btn-header">
+                    <a href="#" class="button-login">LOGIN</a>
+                </div>
+            </div>
+        </div>
+    </header>
+
+    <main>
