6921a7b525cee498f4957279277afdd0bacddfd5
OJhonnys <ojhonnydasilva@gmail.com>
2025-10-14 21:21:31 -0300

    Begin homepage layout (Hero and About completed)

---
+++ index.php
@@ -0,0 +1,38 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Ultra Construction | Home </title>
+
+    <link rel="stylesheet" href="assets/css/style.css">
+</head>
+<body>
+    <!-- header -->
+    <?php include 'header.php'; ?>
+
+    <!-- index -->
+    <section id="hero" class="hero-section">
+        <div class="hero-content">
+            <h1>BEYOND CONSTRUCTION. WE BUILD TRUST.</h1>
+            <p>Raising Standards In Every Project, Delivering Quality That Speaks For Itself.</p>
+            <a href="contact.php" class="hero-cta-btn">CONTACT US</a>
+            <div class="hero-slideshow"> <!-- slideshow is the hero background-->
+                <a href="#"><img src="/assets/images/img-jacksonville.png" alt="City of Jacksonville"></a>
+                <a href="#"><img src="/assets/images/img-boston.jpg" alt="City of Boston"></a>
+                <a href="#"><img src="/assets/images/img-miami.jpg" alt="City of Miami"></a>
+            </div>
+        </div>
+    </section>
+
+    <!-- about section -->
+     <section id="about" class="about-section">
+        <div class="about-container">
+            <h2>BUILDING WITH EXCELLENCE ACROSS THE U.S.</h2>
+            
+
+        </div>
+
+     </section>
+</body>
+</html>
