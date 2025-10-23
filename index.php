6d908e0790d180b50ba600d45e9b03efb8e6dc8b
OJhonnys <ojhonnydasilva@gmail.com>
2025-10-22 22:20:48 -0300

    working on index html, about section, commitment section, projects section and services section

--- index.php
+++ index.php
@@ -18,21 +18,124 @@
             <p>Raising Standards In Every Project, Delivering Quality That Speaks For Itself.</p>
             <a href="contact.php" class="hero-cta-btn">CONTACT US</a>
             <div class="hero-slideshow"> <!-- slideshow is the hero background-->
-                <a href="#"><img src="/assets/images/img-jacksonville.png" alt="City of Jacksonville"></a>
-                <a href="#"><img src="/assets/images/img-boston.jpg" alt="City of Boston"></a>
-                <a href="#"><img src="/assets/images/img-miami.jpg" alt="City of Miami"></a>
+                <img src="/assets/images/img-jacksonville.png" alt="City of Jacksonville">
+                <img src="/assets/images/img-boston.jpg" alt="City of Boston">
+                <img src="/assets/images/img-miami.jpg" alt="City of Miami">
             </div>
         </div>
     </section>
 
     <!-- about section -->
-     <section id="about" class="about-section">
+    <section id="about" class="about-section">
         <div class="about-container">
             <h2>BUILDING WITH EXCELLENCE ACROSS THE U.S.</h2>
-            
+            <div class="about-left">
+                <img src="/assets/images/united-state-of-america-usa-flag.png" alt="United States Flag">
+                <div class="text-about-home">
+                    <p>Ultra Construction has been building reliable structures and event facilities across Florida, Virginia, and Texas, becoming a trusted partner for clients in different industries.
+                    Over the years, we have completed hundreds of projects, from large-scale event stages to specialized commercial builds, always combining precision, safety, and innovation.</p>
+                    <p>Our presence in multiple states allows us to serve a wide range of clients, adapting to each region's unique needs while maintaining the same high standard of quality. With a growing portfolio of completed projects and cities served, Ultra Construction continues to expand its footprint, delivering solutions that bring people and experiences together.</p>
+                </div>
+                <a href="/contact.php" class="cta-btn-quote">GET A QUOTE</a>
+            </div>
+            <div class="about-right-home">
+                <div class="stats-grid-home">
+                    <div class="stat-item">
+                        <span class="stat-number">000+</span>
+                        <p class="stat-label">Projects Completed</p>
+                    </div>
+                    <div class="stat-item">
+                        <span class="stat-number">00+</span>
+                        <p class="stat-label">Cities Served</p>
+                    </div>
+                </div>
+                <img src="/assets/images/ultra-van.png" alt="White Ford Van">
+            </div>
+        </div>
+    </section>
+    
+    <!-- commitment section -->
+    <section id="commitment" class="commitment-section">
+        <div class="commitment-container">
+            <h3>OUR COMMITMENT TO EXCELLENCE</h3>
+            <p class="commitment-intro">At Ultra Construction, what sets us apart is the combination of a Specialized Team with efficient Project Management, ensuring impeccable Quality & Finishing in every detail. Throughout our dedicated Custom Service, we transform your vision into reality, delivering a final result that exceeds your expectations from start to finish.</p>
+
+            <div class="commitment-grid">
+                <span class="icon"></span> <h4>QUALITY & FINISHING</h4> <!-- image on background -->
+            </div>
+            <div class="commitment-grid">
+                <span class="icon"></span> <h4>PROJECT MANAGEMENT</h4> <!-- image on background -->
+            </div>
+            <div class="commitment-grid">
+                <span class="icon"></span> <h4>SPECIALIZED TEAM</h4> <!-- image on background -->
+            </div>
+            <div class="commitment-grid">
+                <span class="icon"></span> <h4>CUSTOM SERVICE</h4> <!-- image on background -->
+            </div>                                    
+        </div>
+        <a href="/contact.php" class="cta-btn">CONTACT US</a>
+    </section>
+    
+    <!-- projects section -->
+    <section id="projects" class="projects-section">
+        <div class="projects-container">
+            <h2>OUR RECENT PROJECTS</h2>
+            <div class="project-gallery"> <!-- PUT HERE 6 IMAGES CARDS -->
+                <div class="project-card"> 
+                    <img src="#" alt="#"> <!-- PROJECT 1 ADD IMAGE -->
+                    <div class="project-info">
+                        <h4>PROJECT NAME</h4>
+                        <P>LOCATION</P>
+                    </div>
+                </div>
+                <div class="project-card"> 
+                    <img src="#" alt="#"> <!-- PROJECT 2 ADD IMAGE -->
+                    <div class="project-info">
+                        <h4>PROJECT NAME</h4>
+                        <P>LOCATION</P>
+                    </div>
+                </div>
+                <div class="project-card"> 
+                    <img src="#" alt="#"> <!-- PROJECT 3 ADD IMAGE -->
+                    <div class="project-info">
+                        <h4>PROJECT NAME</h4>
+                        <P>LOCATION</P>
+                    </div>
+                </div>
+                    <div class="project-card"> 
+                    <img src="#" alt="#"> <!-- PROJECT 4 ADD IMAGE -->
+                    <div class="project-info">
+                        <h4>PROJECT NAME</h4>
+                        <P>LOCATION</P>
+                    </div>
+                </div>
+                    <div class="project-card"> 
+                    <img src="#" alt="#"> <!-- PROJECT 5 ADD IMAGE -->
+                    <div class="project-info">
+                        <h4>PROJECT NAME</h4>
+                        <P>LOCATION</P>
+                    </div>
+                </div>
+                <div class="project-card"> 
+                    <img src="#" alt="#"> <!-- PROJECT 6 ADD IMAGE -->
+                    <div class="project-info">
+                        <h4>PROJECT NAME</h4>
+                        <P>LOCATION</P>
+                    </div>
+                </div>                                        
+            </div>
+            <a href="/projects.php" class="cta-btn-projects">VIEW ALL PROJECTS</a>
+        </div>
+    </section>  
+
+    <!-- services section -->
+    <section id="services" class="services-section">
+        <div class="services container">
+            <h2>WHAT WE DO</h2>
+            <p class="services-section-subtitle">From commercial buildings to residential developments, we bring visions to life with unmatched quality.</p>
 
         </div>
 
-     </section>
+    </section> 
 </body>
 </html>
